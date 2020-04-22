6bf552ba60a7d2fe7d74cf4f55c220f39e448294
Berry Le <60536597+BerryLe@users.noreply.github.com>
2020-04-22 16:37:52 -0500

    Update blue-snek.py

--- blue-snek.py
+++ blue-snek.py
@@ -1,7 +1,7 @@
 from flask_frozen import Freezer
 from pools import app
 
-blue-snek = Freezer(app)
+blue_snek = Freezer(app)
 
 if __name__ == '__main__':
-    blue-snek.freeze()
+    blue_snek.freeze()
