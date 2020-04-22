3404667662e7c770daca8ba2a74cee76c4ad2022
Berry Le <60536597+BerryLe@users.noreply.github.com>
2020-04-22 15:40:24 -0500

    Create pools.py

---
+++ pools.py
@@ -0,0 +1,27 @@
+import requests
+from flask import Flask, request, render_template
+import json
+from xml.etree import ElementTree as ET
+
+# not same origin so can't use only javascript
+pool_info_data = requests.get("https://raw.githubusercontent.com/devdattakulkarni/elements-of-web-programming/master/data/austin-pool-timings.xml")
+pool_info_root = ET.fromstring(pool_info_data.text)
+
+app = Flask(__name__)
+
+# pools table page
+@app.route("/pools")
+def greet():
+    allpools = []
+    for pool in pool_info_root.findall("row"):
+        pooly = {"Pool_name": pool.find("pool_name").text, "Pool_type": pool.find("pool_type").text, "Status": pool.find("status").text, "Open_date": pool.find("open_date").text, "Weekday": pool.find("weekday").text, "Weekend": pool.find("weekend").text}
+        allpools.append(pooly)
+    return json.dumps(allpools)
+
+# welcome page
+@app.route("/")
+def social_distancing():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
