46f9fc4ea5cf0768c1102f34dc4d2ac2b1b16325
Berry Le <60536597+BerryLe@users.noreply.github.com>
2020-04-22 21:41:17 -0500

    Update validate.js

--- static/js/validate.js
+++ static/js/validate.js
@@ -4,33 +4,33 @@ function validateData() {
 	//status test
 	var status = inputs["status"].value;
 	var isstatus = ["Closed", "Open", "In Renovation"];
-	//phone test
-	var phone = inputs["phone"].value;
-	var isphone = /^\d{10}$/;
+	//weekday test
+	var wkday = inputs["wkday"].value;
+	var iswkday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 	//type test
 	var type = inputs["pool_type"].value;
 	var istype = ["Neighborhood", "University", "Community"];
 
 	// if all is valid, return true
-	if (isstatus.indexOf(status) != -1 && phone.match(isphone) && istype.indexOf(type) != -1) {
+	if (isstatus.indexOf(status) != -1 && iswkday.indexOf(wkday) != -1 && istype.indexOf(type) != -1) {
 		return true;
 	}
 	// invalid status
-	else if (isstatus.indexOf(status) == -1){
+	else if (isstatus.indexOf(status) == -1) {
 		text = "Invalid status. Has to be Closed, Open, or In Renovation.";
 		document.getElementById("error").innerHTML = text;
 		console.log(text);
 		return false;
 	}
-	// invalid phone
-  	else if (phone.match(isphone) == null){
-		text = "Invalid phone number. Has to be a 10 digit number without hyphens.";
+	// invalid weekday
+  	else if (iswkday.indexOf(wkday) == -1) {
+		text = "Invalid weekday closure. Has to be a weekday.";
 		document.getElementById("error").innerHTML = text;
 		console.log(text);
 		return false;
 	}
 	// invalid type
-	else if (istype.indexOf(type) == -1){
+	else if (istype.indexOf(type) == -1) {
 		text = "Invalid pool type. Has to be Neighborhood, University, or Community.";
 		document.getElementById("error").innerHTML = text;
 		console.log(text);
