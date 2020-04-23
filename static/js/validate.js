2f5c7651f67e66e5ed3626df726b819bb0e4fd8a
Berry Le <60536597+BerryLe@users.noreply.github.com>
2020-04-22 22:10:46 -0500

    Update validate.js

--- static/js/validate.js
+++ static/js/validate.js
@@ -1,27 +1,27 @@
 function validateData() {
 	var inputs = document.getElementById("pools_form").elements;
 
-	//status test
-	var status = inputs["status"].value;
-	var isstatus = ["Closed", "Open", "In Renovation"];
 	//type test
 	var type = inputs["pool_type"].value;
 	var istype = ["Neighborhood", "University", "Community"];
+	//status test
+	var status = inputs["status"].value;
+	var isstatus = ["Closed", "Open", "In Renovation"];
 
 	// if all is valid, return true
-	if (isstatus.indexOf(status) != -1 && istype.indexOf(type) != -1) {
+	if (istype.indexOf(type) != -1 && isstatus.indexOf(status) != -1) {
 		return true;
-	}
-	// invalid status
-	else if (isstatus.indexOf(status) == -1) {
-		text = "Invalid status. Has to be Closed, Open, or In Renovation.";
-		document.getElementById("error").innerHTML = text;
-		console.log(text);
-		return false;
 	}
 	// invalid type
 	else if (istype.indexOf(type) == -1) {
 		text = "Invalid pool type. Has to be Neighborhood, University, or Community.";
+		document.getElementById("error").innerHTML = text;
+		console.log(text);
+		return false;
+	}
+	// invalid status
+	else if (isstatus.indexOf(status) == -1) {
+		text = "Invalid status. Has to be Closed, Open, or In Renovation.";
 		document.getElementById("error").innerHTML = text;
 		console.log(text);
 		return false;
